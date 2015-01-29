0d7492a410e7fee813b589753a6ff5f3d2d01c56
ED <ed@balcony.com>
2015-01-29 15:09:14 -0600

    modifying bound values after closure

--- Mvc5TestBed.JasmineTests/Scripts/closures/conceptOfClosureSpec.js
+++ Mvc5TestBed.JasmineTests/Scripts/closures/conceptOfClosureSpec.js
@@ -1,64 +1,66 @@
 ﻿/// <reference path="../jasmine/jasmine.js" />
 /// <reference path="conceptOfClosure.js" />
 
-describe("testClosure1", function () {
-    it("should return value from internal variable", function () {
-        var expected = 4;
-        var result = testClosure1();
-        expect(result).toEqual(expected);
+describe("conceptOfClosure", function () {
+    describe("testClosure1", function () {
+        it("should return value from internal variable", function () {
+            var expected = 4;
+            var result = testClosure1();
+            expect(result).toEqual(expected);
+        });
+        it("does not make internal variable accessible", function () {
+            var result = testClosure1();
+            expect(result.x).not.toBeDefined();
+        });
     });
-    it("does not make internal variable accessible", function () {
-        var result = testClosure1();
-        expect(result.x).not.toBeDefined();
-    });
-});
 
-describe("testClosure2", function () {
-    it("inner function can access outer function scope", function () {
-        var expected = 4;
-        var result = testClosure1();
-        expect(result).toEqual(expected);
+    describe("testClosure2", function () {
+        it("inner function can access outer function scope", function () {
+            var expected = 4;
+            var result = testClosure1();
+            expect(result).toEqual(expected);
+        });
     });
-});
 
-describe("buildCoveTicketMaker", function () {
-    it("binds (encloses) parameter to anonymous closure 1", function () {
-        //stored function does not reveal its closure but it's there
-        var getSubmarineTicket = buildCoveTicketMaker("Submarine");
-        //getSubmarineTicket is now a function with the value:
-        //function (name) {
-        //return "Here is your transportation ticket via the " + transport + ".\n" +
-        //    "Welcome to the Cold Closures Cove, " + name + "!";
-        //}
-        // but transport has already been bound to "Submarine"
-        expect(getSubmarineTicket("Mario"))
-            .toEqual("Here is your transportation ticket via the Submarine.\n" +
-                "Welcome to the Cold Closures Cove, Mario!");
-    });
-    it("binds (encloses) parameter to anonymous closure 2", function () {
-        //stored function does not reveal its closure but it's there
-        var getBattleshipTicket = buildCoveTicketMaker("Battleship");
-        //getBattleshipTicket is now a function with the value:
-        //function (name) {
-        //return "Here is your transportation ticket via the " + transport + ".\n" +
-        //    "Welcome to the Cold Closures Cove, " + name + "!";
-        //}
-        // but transport has already been bound to "Battleship"
-        expect(getBattleshipTicket("Luigi"))
-            .toEqual("Here is your transportation ticket via the Battleship.\n" +
-                "Welcome to the Cold Closures Cove, Luigi!");
-    });
-    it("binds (encloses) parameter to anonymous closure 3", function () {
-        //stored function does not reveal its closure but it's there
-        var getGiantSeagullTicket = buildCoveTicketMaker("Giant Seagull");
-        //getGiantSeagullTicket is now a function with the value:
-        //function (name) {
-        //return "Here is your transportation ticket via the " + transport + ".\n" +
-        //    "Welcome to the Cold Closures Cove, " + name + "!";
-        //}
-        // but transport has already been bound to "Giant Seagull"
-        expect(getGiantSeagullTicket("Bowser"))
-            .toEqual("Here is your transportation ticket via the Giant Seagull.\n" +
-                    "Welcome to the Cold Closures Cove, Bowser!");
+    describe("buildCoveTicketMaker", function () {
+        it("binds (encloses) parameter to anonymous closure 1", function () {
+            //stored function does not reveal its closure but it's there
+            var getSubmarineTicket = buildCoveTicketMaker("Submarine");
+            //getSubmarineTicket is now a function with the value:
+            //function (name) {
+            //return "Here is your transportation ticket via the " + transport + ".\n" +
+            //    "Welcome to the Cold Closures Cove, " + name + "!";
+            //}
+            // but transport has already been bound to "Submarine"
+            expect(getSubmarineTicket("Mario"))
+                .toEqual("Here is your transportation ticket via the Submarine.\n" +
+                    "Welcome to the Cold Closures Cove, Mario!");
+        });
+        it("binds (encloses) parameter to anonymous closure 2", function () {
+            //stored function does not reveal its closure but it's there
+            var getBattleshipTicket = buildCoveTicketMaker("Battleship");
+            //getBattleshipTicket is now a function with the value:
+            //function (name) {
+            //return "Here is your transportation ticket via the " + transport + ".\n" +
+            //    "Welcome to the Cold Closures Cove, " + name + "!";
+            //}
+            // but transport has already been bound to "Battleship"
+            expect(getBattleshipTicket("Luigi"))
+                .toEqual("Here is your transportation ticket via the Battleship.\n" +
+                    "Welcome to the Cold Closures Cove, Luigi!");
+        });
+        it("binds (encloses) parameter to anonymous closure 3", function () {
+            //stored function does not reveal its closure but it's there
+            var getGiantSeagullTicket = buildCoveTicketMaker("Giant Seagull");
+            //getGiantSeagullTicket is now a function with the value:
+            //function (name) {
+            //return "Here is your transportation ticket via the " + transport + ".\n" +
+            //    "Welcome to the Cold Closures Cove, " + name + "!";
+            //}
+            // but transport has already been bound to "Giant Seagull"
+            expect(getGiantSeagullTicket("Bowser"))
+                .toEqual("Here is your transportation ticket via the Giant Seagull.\n" +
+                        "Welcome to the Cold Closures Cove, Bowser!");
+        });
     });
 });
---
+++ Mvc5TestBed.JasmineTests/Scripts/closures/warningMaker.js
@@ -0,0 +1,24 @@
+﻿function warningMaker(obstacle) {
+    return function (number, location) {
+        return ("Beware! There have been " +
+              obstacle +
+              " sightings in the Cove today!\n" +
+              number +
+              " " +
+              obstacle +
+              "(s) spotted at the " +
+              location +
+              "!"
+             );
+    };
+}
+
+//var killerPenguinAlert = warningMaker("killer penguin");
+//var polarBearAlert = warningMaker("polar bear");
+//var icebergAlert = warningMaker("iceberg");
+//var flashBlizzardAlert = warningMaker("flash blizzard");
+//var snowYetiAlert = warningMaker("snow yeti");
+
+
+//killerPenguinAlert(6, "Ice Caves");
+//snowYetiAlert(1, "Blizzard Beach");
