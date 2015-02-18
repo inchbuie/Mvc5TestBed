8d8d2a8495b0fb15465f616f006adb322f0d3970
ED <ed@balcony.com>
2015-02-18 13:53:32 -0600

    Html5 drag-drop and upload demos

--- Mvc5TestBed.MyMvcWebApp/Controllers/PictureUploadController.cs
+++ Mvc5TestBed.MyMvcWebApp/Controllers/PictureUploadController.cs
@@ -42,5 +42,39 @@ namespace Mvc5TestBed.MyMvcWebApp.Controllers
             };
             return View("Index", o);
         }
+
+        public ActionResult JcropBasicDemo()
+        {
+            return View();
+        }
+
+        public ActionResult DragUpload(UploadThingy info)
+        {
+            return View(info);
+        }
+
+        [HttpPost]
+        public ActionResult DragUpload(IEnumerable<HttpPostedFileBase> pictureFiles)
+        {
+            string pathToSave = null;
+            foreach (var pictureFile in pictureFiles)
+            {
+                if (null == pictureFile || pictureFile.ContentLength == 0)
+                    continue;
+                pathToSave = Path.Combine(
+                   AppDomain.CurrentDomain.BaseDirectory,
+                   Path.GetFileName(pictureFile.FileName));
+                //pictureFile.SaveAs(pathToSave);
+
+                //for now just do 1...
+                break;
+            }
+            var o = new UploadThingy()
+            {
+                //as test, ignore path &hard-code this                
+                ImgUrl = Url.Content("~/images/darth.png")
+            };
+            return View("DragUpload", o);
+        }
     }
 }
