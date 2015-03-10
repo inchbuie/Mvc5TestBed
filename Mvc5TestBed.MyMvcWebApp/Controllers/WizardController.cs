0d1e63960ddbe3f7f0167bf5f96b3bb41058af4e
ED <ed@balcony.com>
2015-03-10 17:20:23 -0500

    Adding MVC wizard example

---
+++ Mvc5TestBed.MyMvcWebApp/Controllers/WizardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5TestBed.MyMvcWebApp.Controllers
+{
+    public class WizardController : Controller
+    {
+        // GET: Loading
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
