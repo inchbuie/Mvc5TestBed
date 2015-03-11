9d877eb3247ce5b063cbb6aa9704132f8645f6f3
ED <ed@balcony.com>
2015-03-11 15:49:02 -0500

    added Sales controller

---
+++ ParentChild.Web/Controllers/SalesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ParentChild.DataLayer;
+using ParentChild.Model;
+
+namespace ParentChild.Web.Controllers
+{
+    public class SalesController : Controller
+    {
+        private SalesContext _salesContext;
+
+        public SalesController()
+        {
+            _salesContext = new SalesContext();
+        }
+
+        // GET: SalesOrders
+        public ActionResult Index()
+        {
+            return View(_salesContext.SalesOrders.ToList());
+        }
+
+        // GET: SalesOrders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalesOrder salesOrder = _salesContext.SalesOrders.Find(id);
+            if (salesOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salesOrder);
+        }
+
+        // GET: SalesOrders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SalesOrders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CustomerName,PONumber")] SalesOrder salesOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                _salesContext.SalesOrders.Add(salesOrder);
+                _salesContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(salesOrder);
+        }
+
+        // GET: SalesOrders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalesOrder salesOrder = _salesContext.SalesOrders.Find(id);
+            if (salesOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salesOrder);
+        }
+
+        // POST: SalesOrders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CustomerName,PONumber")] SalesOrder salesOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                _salesContext.Entry(salesOrder).State = EntityState.Modified;
+                _salesContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(salesOrder);
+        }
+
+        // GET: SalesOrders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalesOrder salesOrder = _salesContext.SalesOrders.Find(id);
+            if (salesOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salesOrder);
+        }
+
+        // POST: SalesOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SalesOrder salesOrder = _salesContext.SalesOrders.Find(id);
+            _salesContext.SalesOrders.Remove(salesOrder);
+            _salesContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _salesContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
